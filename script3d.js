a5c88746b058d8168c993977bc104e017c65fcbf
SkɏNɨǥħŧ <luke.baffait@yahoo.com>
2025-08-23 23:54:33 +0200

    Add files via upload

--- script3d.js
+++ script3d.js
@@ -180,7 +180,7 @@ function updateGradient() {
   const gradient = document.getElementById('gradient');
   gradient.innerHTML = '';
 
-  const gridSize = 768;
+  const gridSize = 750;
   const blockSize = gridSize / size;
   gradient.style.display = 'grid';
   gradient.style.gridTemplateColumns = `repeat(${size}, ${blockSize}px)`;
